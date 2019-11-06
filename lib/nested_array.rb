9b88d1633e239072d694064570d5b992a4756d13
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-06 01:52:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -27,7 +27,7 @@ def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
-  new_array = [["Don Gately",
+  new_array.sort = [["Don Gately",
   "Joelle van Dyne",
   "Pat M.",
   "Kate Gompert",
